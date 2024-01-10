b3fcdd945b6b7f36fae87f1adaf11d9b7c6d9352
madoutk07 <129869857+madoutk07@users.noreply.github.com>
2024-01-10 19:55:57 +0100

    v3 TD9-10 debut affichage

--- Dash_interface/interface.py
+++ Dash_interface/interface.py
@@ -1,8 +1,8 @@
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 import pandas as pd
-from dash.exceptions import PreventUpdate 
 
 # Exemple de données
 data = [
@@ -15,18 +15,18 @@ df = pd.DataFrame(data)
 
 app = dash.Dash(__name__)
 
-# Layout principal avec le formulaire de recherche
+
 app.layout = html.Div(style={'backgroundColor': '#f2f2f2', 'padding': '50px', 'height': '100vh'}, children=[
+    html.H1("Corpus", style={'textAlign': 'center', 'color': '#333', 'marginBottom': 20}),
+    
+    # Formulaire de recherche
     html.Div([
-        html.H1("Corpus", style={'textAlign': 'center', 'color': '#333', 'marginBottom': 20}),
-        
         html.Label("Mot_clé:", style={'color': '#333', 'fontSize': 16}),
         dcc.Input(id='keyword_input', type='text', placeholder='Entrez le mot-clé', style={'width': '95%', 'marginBottom': 20, 'padding': '10px', 'fontSize': 14}),
         
         html.Label("Auteur:", style={'color': '#333', 'fontSize': 16}),
         dcc.Input(id='author_input', type='text', placeholder="Entrez le nom de l'auteur", style={'width': '95%', 'marginBottom': 20, 'padding': '10px', 'fontSize': 14}),
       
-    
         html.Label("Source:", style={'color': '#333', 'fontSize': 16}),
         dcc.Dropdown(
             id='genre_dropdown',
@@ -39,12 +39,11 @@ app.layout = html.Div(style={'backgroundColor': '#f2f2f2', 'padding': '50px', 'h
         ),
         
         html.Button('Rechercher', id='search_button', n_clicks=0, style={'backgroundColor': '#4CAF50', 'color': 'white', 'padding': '10px 20px', 'fontSize': 16}),
-        html.Div(id='search_results', style={'marginTop': 20, 'fontSize': 18, 'color': '#333'}),
-        
-    ], style={'maxWidth': '600px', 'margin': 'auto', 'borderRadius': '10px', 'boxShadow': '0px 0px 10px 0px rgba(0,0,0,0.1)'})
-])
+    ], style={'maxWidth': '600px', 'margin': 'auto', 'borderRadius': '10px', 'boxShadow': '0px 0px 10px 0px rgba(0,0,0,0.1)'}),
 
-search_results = []  # Variable pour stocker les résultats de la recherche
+    # Résultats de la recherche
+    html.Div(id='search_results', style={'marginTop': 20, 'fontSize': 18, 'color': '#333'}),
+])
 
 # Callback pour mettre à jour les résultats de la recherche
 @app.callback(
@@ -55,48 +54,19 @@ search_results = []  # Variable pour stocker les résultats de la recherche
      State('genre_dropdown', 'value')]
 )
 def update_search_results(n_clicks, author_query, keyword_query, genre_query):
-    global search_results
-    
-    # if n_clicks == 0:
-    #     raise PreventUpdate  # Aucune mise à jour avant le clic sur le bouton
+    if n_clicks == 0:
+        raise PreventUpdate
 
     filtered_results = [
         result for result in data
-        if (str(author_query) in result['Auteur']) and
-           (keyword_query in result['Mot_clé']) and
+        if (str(author_query) in result['Auteur']) or
+           (keyword_query in result['Mot_clé']) or
            (genre_query == result['Source'])
     ]
 
-    search_results = filtered_results  
+    search_results = [html.P(f"Mot_clé: {result['Mot_clé']} | Auteur : {result['Auteur']} | Source: {result['Source']}") for result in filtered_results]
 
-    return [html.P(f"Auteur: {result['Auteur']} | Mot_clé: {result['Mot_clé']} | Source: {result['Source']}") for result in search_results]
-print("ok")
-
-# Fonction pour générer le layout des résultats de recherche
-# def generate_search_results_layout(results):
-#     return html.Div([
-#         html.H1("Résultats de la Recherche", style={'textAlign': 'center'}),
-#         html.Div([generate_result_link(result) for result in results])
-#     ])
-print ("ok2")
-# def initial_layout():
-#     if not search_results:
-#         return app.layout
-#     else:
-#         return generate_search_results_layout(search_results)
-
-
-def generate_result_link(result):
-    return dcc.Link(
-        html.Div([
-            html.H3(result['Mot_clé'], style={'margin-bottom': '5px'}),  
-            html.P(f"Auteur: {result['Auteur']} | Date: {result['Date']} | Source: {result['Source']}")
-        ]),
-        href=f"/result/{result['Mot_clé']}",  
-        style={'text-decoration': 'none', 'color': 'black'}
-    )
-print ("ok3")
-# app.layout = initial_layout
+    return search_results
 
 if __name__ == '__main__':
     app.run_server(debug=True)
